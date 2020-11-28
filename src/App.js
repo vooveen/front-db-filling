6b78045653ce580a95f5e613fde49d6b8266200a
Sofiane Benmesbah <sofiane.benmes@gmail.com>
2020-11-28 14:28:46 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,219 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Formik, Form, Field , FieldArray} from 'formik';
+import { Button, LinearProgress } from '@material-ui/core';
+import { TextField, Select , Switch} from 'formik-material-ui';
+import {TextField as Timhilti}  from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+ const modules =[
+"Anatomie",
+"Anatomie pathologique",
+"Biochimie",
+"Cardiologie et pathologie vasculaire",
+"Cytologie",
+"Dermatologie",
+"Embryologie",
+"Endocrinologie-Métabolisme",
+"Epidémiologie",
+"Génétique",
+"Gynécologie-Obstétrique",
+"Hématologie",
+"Hépato-gastro-entérologie",
+"Histologie",
+"Immunologie",
+"Maladies infectieuses",
+"Médecine du travail",
+"Médecine légale et toxicologie",
+"Microbiologie",
+"Néphro-urologie",
+"Neurologie",
+"Ophtalmologie",
+"Orthopédie",
+"Oto-rhino-laryngologie",
+"Parasitologie",
+"Pédiatrie",
+"Pharmacologie",
+"Physiologie",
+"Pneumologie",
+"Psychiatrie",
+"Radiologie",
+"Rééducation",
+"Rhumatologie",
+"Cancérologie",
+"Appareil locomoteur",
+"Nutrition",
+"Santé Publique",
+"Stomatologie"
+ ]
+
+
+ 
+
+ 
+ function App() {
+   const [propNumber,setPropNumber]= React.useState(5);
+   const [unchangedValue,SetUnchangedValue] = React.useState({
+     module:'Anatomie',
+     cours:'',
+     synthese:'',
+     enonce:'',
+     comment:'',
+     propositions:Array(propNumber).fill({
+      proposition:'',
+      solution:false
+     }) 
+   })
+   return (
+   <>
+   <p>{JSON.stringify(unchangedValue)}</p>
+   <Timhilti
+          
+          label="Nombre de Proposition"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+         // value={propNumber}
+          variant="outlined"
+          onKeyDown={(e)=>{
+
+            if(e.keyCode === 13) {
+              setPropNumber(e.target.value);
+              
+              SetUnchangedValue({
+                ...unchangedValue,
+               
+                propositions:  Array.from({ length: e.target.value }, () => ({
+                  proposition:'',
+                  solution:false
+                 }))
+              })
+            }
+          }}
+          //onChange={(e)=>}
+        />
+        <p>{propNumber}</p>
+     <Formik
+     enableReinitialize
+       initialValues={unchangedValue}
+       onSubmit={(values, { setSubmitting, setValues}) => {
+     
+           setSubmitting(false);
+           alert(JSON.stringify(values, null, 2));
+           const payload = {...values, enonce: '', comment:'',propositions:  Array.from({ length: 5}, () => ({
+            proposition:'',
+            solution:false
+           }))} 
+             setValues(payload)
+           
+       
+       }}
+     >
+       {({ submitForm, isSubmitting, values }) => (
+         <Form>
+           <Grid container spacing={2}>
+           <Grid item xs={4}>
+             <Field
+             component={Select}
+             name="module"
+             type="module"
+             label="Module"
+             fullWidth
+             variant="outlined"
+           >
+          { modules.map(e=>
+            <MenuItem value={e}>{e}</MenuItem>)}
+            </Field>
+           <Field
+             component={TextField}
+             name="cours"
+             type="cours"
+             label="Cours"
+             fullWidth
+             variant="outlined"
+           />
+           <br />
+           <br />
+           <Field
+             component={TextField}
+             multiline
+             rowsMax={10}
+             name="synthese"
+             type="synthese"
+             label="Synthese"
+             fullWidth
+             variant="outlined"
+           />
+           <br />
+           </Grid>
+           <Grid item xs={8}>
+           <Field
+             component={TextField}
+             multiline
+             rowsMax={10}
+             type="enonce"
+             name="enonce"
+             label="Enoncé"
+             fullWidth
+             variant="outlined"
+           />
+           <br />
+           <Field
+             component={TextField}
+             multiline
+             rowsMax={10}
+             type="comment"
+             name="comment"
+             label="Commentaire"
+             fullWidth
+             variant="outlined"
+           />
+           
+           <Grid container spacing={2}>
+                 {
+                   values.propositions.map((value, index) => (
+                     <>
+                      <Grid item xs={10}>
+                    <Field
+                    component={TextField}
+                    multiline
+                    rowsMax={10}
+                    name={`propositions.${index}.proposition`}
+                    label={`propositions${index+1}`}
+                    fullWidth
+                   variant="outlined"
+                  />
+                  </Grid>
+                  <Grid item xs={2}>
+                  <label>
+                  <Field type="checkbox" name={`propositions.${index}.solution`} />
+                  {`${value.solution}`}
+                </label>
+                </Grid>
+                     </>
+                 ))}
+                 </Grid>
+             
+           
+</Grid>
+           {isSubmitting && <LinearProgress />}
+           <br />
+           <Button
+             variant="contained"
+             color="primary"
+             disabled={isSubmitting}
+             onClick={submitForm}
+           >
+             Submit
+           </Button>
+           </Grid>
+         </Form>
+       )}
+     </Formik>
+     </>
+   );
+ }
 
 export default App;
